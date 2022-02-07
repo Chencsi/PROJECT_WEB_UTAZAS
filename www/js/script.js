7020dc9711b516a4401c8213d4c66c26f9abd642
chencsi <kevinkeeeh@gmail.com>
2022-02-07 22:00:34 +0100

    contact us, nav

--- www/js/script.js
+++ www/js/script.js
@@ -9,15 +9,83 @@ function navFunction(event) {
 // Ez a függvény fut le, amikor betölt az oldal
 function mainFunction() {
     // Linkek listához adása, oldal tetejére görgetés
-    window.scrollTo(0, 0);
     document.querySelector('.navbar-nav a:first-child').classList.add('active');
     for (let i = 0; i < links.childElementCount; i++) {
         navLinks.push(linksChilds[i])
     }
-
 }
 
 // Változók
 let navLinks = [];
 let links = document.getElementById('links');
 let linksChilds = links.children;
+
+let lastPos = 0;
+let currPos = 0;
+let ticking = false;
+
+function scrollFunction() {
+    if (window.scrollY > 10){
+        document.getElementById('nav').classList.add('bg-dark');
+    } else {
+        document.getElementById('nav').classList.remove('bg-dark');
+    }
+}
+
+// function scrollFunction() {
+//     currPos = window.scrollY;
+//     for (let i = 0; i < links.childElementCount; i++) {
+//         if (linksChilds[i].classList.contains('active')){
+//             activeLink = [linksChilds[i], i]
+//         }
+//     }
+//     setTimeout(function() {
+//         if (currPos > lastPos) {
+//             activeLink[0].classList.remove('active');
+//             linksChilds[activeLink[1] + 1].classList.add('active');
+//             // for (let i = 0; i < links.childElementCount; i++) {
+//             //     if (activeLink[1] + 1 != i) {
+//             //         activeLink[i].classList.remove('active');
+//             //     }
+//             // }
+//         } else {
+//             activeLink[0].classList.remove('active');
+//             linksChilds[activeLink[1] - 1].classList.add('active');
+//         }
+//         lastPos = window.scrollY;
+//     }, 1000)
+// }
+
+// document.addEventListener('scroll', function(e) {
+//   lastKnownScrollPosition = window.scrollY;
+
+//   if (!ticking) {
+//     window.requestAnimationFrame(function() {
+//       console.log(lastKnownScrollPosition)
+//       ticking = false;
+//     });
+
+//     ticking = true;
+//   }
+// });
+
+
+// let lastKnownScrollPosition = 0;
+// let ticking = false;
+// var elmnt = document.getElementById("ajanlataink");
+// function doSomething(scrollPos) {
+
+// }
+// document.addEventListener('scroll', function(e) {
+//   lastKnownScrollPosition = window.scrollY;
+
+//   elmnt.scrollIntoView();
+//   if (!ticking) {
+//     window.requestAnimationFrame(function() {
+//       alert('asd');
+//       ticking = false;
+//     });
+
+//     ticking = true;
+//   }
+// });
